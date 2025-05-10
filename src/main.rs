b8769b8f79eb8f125944f0841aaf42b0d7222cdb
CM <omegajms@gmail.com>
2025-05-10 10:25:44 -0400

    Add relaying of uuid, sending of response messages

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ use uuid::Uuid;
 pub struct Client 
 {
     x: f32,
-    y: f32
+    y: f32,
+    raw_bytes: [u8;44]
 }
 
 impl Client 
@@ -21,8 +22,23 @@ impl Client
         Self
         {
             x:0.0,
-            y:0.0
+            y:0.0,
+            raw_bytes: [0;44]
         }
+    }
+
+    pub fn set_uuid(&mut self, uuid: &String)
+    {
+        let uuid_bytes = uuid.as_bytes(); //36 bytes
+
+        if uuid_bytes.len() != 36
+        {
+            println!("A uuid was not 36 bytes long. It was {}",uuid_bytes.len());
+            return; //bail!
+        }
+
+        let slice_to_overwrite = &mut self.raw_bytes[0..36];
+        slice_to_overwrite.copy_from_slice(&uuid.as_bytes());
     }
 
     pub fn update(&mut self, message: &Message)
@@ -39,11 +55,15 @@ impl Client
                 return;
             }
 
+            //TODO: x/y is just for debugging
             let bytes1: [u8; 4] = data[0..4].try_into().unwrap();
             self.x = f32::from_le_bytes(bytes1);
 
             let bytes2: [u8; 4] = data[4..8].try_into().unwrap();
             self.y = f32::from_le_bytes(bytes2);
+
+            (&mut self.raw_bytes[36..40]).copy_from_slice(&bytes1);
+            (&mut self.raw_bytes[40..44]).copy_from_slice(&bytes2);
         }
     }
 
@@ -52,12 +72,10 @@ impl Client
         println!("{} : at {} x {}",uuid,self.x,self.y);
     }
 
-    /*
-    pub fn from_vec_u8(&self) -> Vec<u8>
+    pub fn as_bytes(&self) -> &[u8;44]
     {
-        vec![1,2,3]
+        &self.raw_bytes
     }
-    */
 }
 
 #[tokio::main]
@@ -65,7 +83,7 @@ async fn main() -> Result<()> {
     let addr = "127.0.0.1:8000".to_string();
     let listener = TcpListener::bind(&addr).await?;
 
-    let peer_db = Arc::new(Mutex::new(HashMap::<String,Client>::new()));
+    let peer_db = Arc::new(Mutex::new(HashMap::<Uuid,Client>::new()));
 
     while let Ok((stream, _)) = listener.accept().await {
         tokio::spawn(handle_connection(stream,peer_db.clone()));
@@ -74,7 +92,7 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-async fn handle_connection(stream: tokio::net::TcpStream, peer_db: Arc<Mutex<HashMap<String,Client>>>) -> Result<()> {
+async fn handle_connection(stream: tokio::net::TcpStream, peer_db: Arc<Mutex<HashMap<Uuid,Client>>>) -> Result<()> {
 
     let mut ws_stream = accept_async(stream).await?;
 
@@ -82,7 +100,12 @@ async fn handle_connection(stream: tokio::net::TcpStream, peer_db: Arc<Mutex<Has
 
     {
         let mut db = peer_db.lock().unwrap();
-        db.insert(uuid.to_string().clone(),Client::new());
+
+        let mut client = Client::new();
+        let new_uuid = uuid.to_string().clone();
+        client.set_uuid(&new_uuid);
+
+        db.insert(uuid,client);
     }
 
     println!("Client connected (WebSocket): {}", uuid);
@@ -94,7 +117,7 @@ async fn handle_connection(stream: tokio::net::TcpStream, peer_db: Arc<Mutex<Has
                 
                 {
                     let mut db = peer_db.lock().unwrap();
-                    match db.get_mut(&uuid.to_string()).as_mut()
+                    match db.get_mut(&uuid).as_mut()
                     {
                         Some(entry) => 
                         {
@@ -105,20 +128,26 @@ async fn handle_connection(stream: tokio::net::TcpStream, peer_db: Arc<Mutex<Has
                     };
                 }
 
-                /*
-                let maybe_data :  Option<Vec<u8>>;
+                let mut message_queue : Vec<Message> = Vec::<Message>::new(); //TODO: presize?
 
-                match maybe_data
                 {
-                    Some(d) => {
-                        //println!("Sending back data we saved.");
-                        ws_stream.send(Message::Binary(d)).await?;
-                    },
-                    None => {}
+                    for (peer_uuid,peer) in peer_db.lock().unwrap().iter()
+                    {
+                        if peer_uuid == &uuid
+                        {
+                            continue;
+                        }
+
+                        message_queue.push(Message::Binary(peer.as_bytes().to_vec()));
+                    }
                 }
-                */
+
+                for message in message_queue.into_iter()
+                {
+                    ws_stream.send(message).await?;
+                }
             },
-            Err(e) => {
+            Err(_) => {
                 //TODO
             }
         };
@@ -126,7 +155,7 @@ async fn handle_connection(stream: tokio::net::TcpStream, peer_db: Arc<Mutex<Has
 
     {
         let mut db = peer_db.lock().unwrap();
-        db.remove(&uuid.to_string());
+        db.remove(&uuid);
     }
 
     println!("Client disconnected (WebSocket): {}", uuid);
