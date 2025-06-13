720be01dcae6143faf66ce57a789659ef67082ac
CM <omegajms@gmail.com>
2025-06-13 17:01:32 -0400

    Fix message parsing for types

--- src/main.rs
+++ src/main.rs
@@ -10,79 +10,25 @@ use uuid::Uuid;
 
 pub struct Client 
 {
-    x: f32,
-    y: f32,
-    raw_bytes: [u8;44],
-    departed_peer_messages: Vec<Message>
+    uuid: Uuid,
+    outbound_messages: Vec<Message>
 }
 
 impl Client 
 {
-    pub fn new() -> Self
+    pub fn new(uuid: &Uuid) -> Self
     {
         Self
         {
-            x:0.0,
-            y:0.0,
-            raw_bytes: [0;44],
-            departed_peer_messages: Vec::<Message>::new()
+            uuid: uuid.clone(), //Client UUID
+            outbound_messages: Vec::<Message>::new()
         }
-    }
-
-    pub fn set_uuid(&mut self, uuid: &String)
-    {
-        let uuid_bytes = uuid.as_bytes(); //36 bytes
-
-        if uuid_bytes.len() != 36
-        {
-            println!("A uuid was not 36 bytes long. It was {}",uuid_bytes.len());
-            return; //bail!
-        }
-
-        let slice_to_overwrite = &mut self.raw_bytes[0..36];
-        slice_to_overwrite.copy_from_slice(&uuid.as_bytes());
-    }
-
-    pub fn update(&mut self, message: &Message)
-    {
-        if let Message::Binary(data) = message
-        {
-            if !message.is_binary() 
-            {
-                return;
-            }
-
-            if message.len() != 8
-            {
-                return;
-            }
-
-            //TODO: x/y is just for debugging
-            let bytes1: [u8; 4] = data[0..4].try_into().unwrap();
-            self.x = f32::from_le_bytes(bytes1);
-
-            let bytes2: [u8; 4] = data[4..8].try_into().unwrap();
-            self.y = f32::from_le_bytes(bytes2);
-
-            (&mut self.raw_bytes[36..40]).copy_from_slice(&bytes1);
-            (&mut self.raw_bytes[40..44]).copy_from_slice(&bytes2);
-        }
-    }
-
-    pub fn print(&self, uuid: &String)
-    {
-        println!("{} : at {} x {}",uuid,self.x,self.y);
-    }
-
-    pub fn as_bytes(&self) -> &[u8;44]
-    {
-        &self.raw_bytes
     }
 
     pub fn inform_of_departed_peer(&mut self, uuid: &Uuid)
     {
-        let string_uuid = uuid.to_string();
-        let uuid_bytes = string_uuid.as_bytes(); //36 bytes. NB: copies, $
+        let binding = uuid.to_string();
+        let uuid_bytes = binding.as_bytes(); //36 bytes. NB: copies, $
 
         if uuid_bytes.len() != 36
         {
@@ -90,14 +36,55 @@ impl Client
             return; //bail!
         }
 
-        //A departed peer message is 36 bytes (just a UUID)
-        self.departed_peer_messages.push(Message::Binary(uuid_bytes.to_vec()));
+        let message_type : i32 = 2;
+        let mut message_bytes = Vec::<u8>::new();
+
+        message_bytes.append(&mut message_type.to_le_bytes().to_vec());
+        message_bytes.append(&mut uuid_bytes.to_vec());
+
+        //A departed peer message is 4 + 36 bytes
+        self.outbound_messages.push(Message::Binary(message_bytes));
     }
 
-    pub fn take_departed_peer_messages(&mut self) -> Vec<Message>
+    pub fn receive_inbound_message(&mut self, message: &Message)
     {
-        std::mem::take(&mut self.departed_peer_messages)
+        //TODO: maybe later dont clone, this wont scale
+        self.outbound_messages.push(message.clone());
     }
+
+    pub fn take_outbound_messages(&mut self) -> Vec<Message>
+    {
+        std::mem::take(&mut self.outbound_messages)
+    }
+}
+
+fn create_inbound_message(uuid: &Uuid, message: &Message) -> Option<Message>
+{
+    let binding = uuid.to_string();
+    let mut uuid_bytes = binding.as_bytes(); //36 bytes. NB: copies, $
+
+    if uuid_bytes.len() != 36
+    {
+        println!("A uuid was not 36 bytes long. It was {}",uuid_bytes.len());
+        return None; //bail!
+    }
+
+    if let Message::Binary(bytes) = message
+    {
+        if bytes.len() < 4
+        {
+            //No message type
+            return None; //bail
+        }
+
+        let mut updated_bytes = Vec::<u8>::new();
+        updated_bytes.append(&mut bytes[0..4].to_vec());
+        updated_bytes.append(&mut uuid_bytes.to_vec());
+        updated_bytes.append(&mut bytes[4..].to_vec());
+        return Some(Message::Binary(updated_bytes));
+    }
+    
+    None
 }
 
 #[tokio::main]
@@ -122,11 +109,7 @@ async fn handle_connection(stream: tokio::net::TcpStream, peer_db: Arc<Mutex<Has
 
     {
         let mut db = peer_db.lock().unwrap();
-
-        let mut client = Client::new();
-        client.set_uuid(&uuid.to_string()); //NB: copies
-
-        db.insert(uuid,client);
+        db.insert(uuid,Client::new(&uuid));
     }
 
     println!("Client connected (WebSocket): {}", uuid);
@@ -141,40 +124,50 @@ async fn handle_connection(stream: tokio::net::TcpStream, peer_db: Arc<Mutex<Has
                     //Connection is closing...
                     break;
                 }
-                
-                let mut message_queue : Vec<Message> = Vec::<Message>::new(); //TODO: presize?
-                //NB: queue is set below, don't append things to it before this occurs.
+
+                //Create an inbound message for this message
+                //This tacks the UUID bytes onto the message
+                let inbound_message = create_inbound_message(&uuid,&msg);
+
+                {
+                    //TODO: maybe later don't do this on EVERY inbound message
+                    //Tell all other clients about the message this client sent
+
+                    if inbound_message.is_some()
+                    {
+                        let inbound_message_unwrapped = inbound_message.unwrap();
+
+                        for (peer_uuid,peer) in peer_db.lock().unwrap().iter_mut()
+                        {
+                            if peer_uuid == &uuid
+                            {
+                                continue;
+                            }
+
+                            //TODO: this can be very expensive since it's copying the message for every single peer.
+                            peer.receive_inbound_message(&inbound_message_unwrapped);
+                        }
+                    }
+                }
+
+
+                //Send back all messages pending for this client
+                let mut outbound_messages = Vec::<Message>::new();
 
                 {
                     let mut db = peer_db.lock().unwrap();
                     match db.get_mut(&uuid).as_mut()
                     {
-                        Some(entry) => 
+                        Some(client) => 
                         {
-                            //Update this client in the server
-                            entry.update(&msg);
-
-                            //Drain the departed peer messages into the queue
-                            message_queue = entry.take_departed_peer_messages();
+                            outbound_messages = client.take_outbound_messages();
                         },
                         None => {}
                     };
                 }
 
-                {
-                    //Enqueue an update for each connected peer
-                    for (peer_uuid,peer) in peer_db.lock().unwrap().iter()
-                    {
-                        if peer_uuid == &uuid
-                        {
-                            continue;
-                        }
-
-                        message_queue.push(Message::Binary(peer.as_bytes().to_vec()));
-                    }
-                }
-
-                for message in message_queue.into_iter()
+                //Send back all messages which are pending for this client
+                for message in outbound_messages
                 {
                     ws_stream.send(message).await?;
                 }
@@ -185,7 +178,20 @@ async fn handle_connection(stream: tokio::net::TcpStream, peer_db: Arc<Mutex<Has
         };
     }
 
-    println!("Client disconnected (WebSocket): {}", uuid);
+    //All the peers in the map need to know this peer is leaving now.
+    //When they next poll, they'll get the update 
+    {
+        for (peer_uuid,peer) in peer_db.lock().unwrap().iter_mut()
+        {
+            if peer_uuid == &uuid
+            {
+                continue;
+            }
+
+            println!("Informed client {} of departing peer {}", peer_uuid, uuid);
+            peer.inform_of_departed_peer(&uuid);
+        }
+    }
 
     //Remove this peer from the DB
     {
@@ -193,16 +199,7 @@ async fn handle_connection(stream: tokio::net::TcpStream, peer_db: Arc<Mutex<Has
         db.remove(&uuid);
     }
 
-    //All the peers in the map need to know this peer left now.
-    //When they next poll, they'll get the update 
-    {
-        for (peer_uuid,peer) in peer_db.lock().unwrap().iter_mut()
-        {
-
-            println!("Informed client {} of departing peer {}", peer_uuid, uuid);
-            peer.inform_of_departed_peer(&uuid);
-        }
-    }
+    println!("Peer departed (WebSocket closed): {}", uuid);
 
     Ok(())
 }
